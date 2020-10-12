5899e181685e14cc518a9028b77b1f45105d99e4
loululin <loululin@163.com>
2020-10-12 09:38:55 +0800

    vpnstart shell

---
+++ vpnstart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+echo "loululin123"|sudo -S nohup openvpn --config /home/loulvlin/MyDoc/myovpen/config.ovpn  --ca /home/loulvlin/MyDoc/myovpen/ca.crt --cert /home/loulvlin/MyDoc/myovpen/vsc-2zey3gm0ez8xx1xo4b70g.crt --key /home/loulvlin/MyDoc/myovpen/vsc-2zey3gm0ez8xx1xo4b70g.key 2>&1 &
